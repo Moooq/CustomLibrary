042486ecc4263bccadda5979dc3305b10b42aa52
moqian <jam.mooq@foxmail.com>
2019-05-16 14:34:51 +0800

    添加flowlayout;

--- app/src/main/java/com/mooq/customlibrary/MainActivity.java
+++ app/src/main/java/com/mooq/customlibrary/MainActivity.java
@@ -3,11 +3,20 @@ package com.mooq.customlibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		FlowLayout flTest = (FlowLayout) findViewById(R.id.fl_test);
+		List<String> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++){
+			list.add("item"+i);
+		}
+		flTest.setData(list);
 	}
 }
